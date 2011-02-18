ca81e8bc72212b6a4e9206eef1ef6b2a29732aa4
Adam Roderick <aroder@gmail.com>
2011-02-18 16:53:21 -0700

    map loading in test page

--- dojoApp/js/routeFinder/routing/Map.js
+++
@@ -1,8 +0,0 @@
-dojo.provide('routeFinder.routing.Map');
-
-
-
-dojo.declare('routeFinder.routing.Map', [dijit._Widget, dijit._Templated], {
-	myvar: 'myvar value',
-	templatePath: dojo.moduleUrl('routeFinder', 'templates/route.html'),
-});
